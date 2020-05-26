42a4c66da829aae35a63a98d8016dcaecc84368d
mango-drive <sebastien.corrigan@gmail.com>
2020-05-25 19:15:13 -0700

    Create board with swappable tiles. onMouseDown causes the board to enter in a mode where the blank tile can be swapped with its neighbour

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Board/>
   );
 }
 
 export default App;
+
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      // outer div for the card
+      <div
+        className={this.props.className}
+        onMouseDown={this.props.onMouseDown}
+        onMouseEnter={this.props.onMouseEnter}
+      >
+        <div className='tile-content'>{this.props.id}</div>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tiles: [
+        [1, 2, 3],
+        [4, 0, 5],
+        [6, 7, 8]
+      ],
+      isMoving: false,
+      swapOrigin: null
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("mouseup", this.handleOnMouseUp);
+  }
+
+  handleOnMouseDown(i, j) {
+    if (!this.state.isMoving && this.state.tiles[i][j] == 0) {
+      this.setState({isMoving: true, swapOrigin: {i, j}})
+    }
+  }
+
+  handleOnMouseEnter(i, j) {
+    if (this.state.isMoving) {
+      this.swapTiles(this.state.swapOrigin, {i, j});
+      this.setState({swapOrigin: {i, j}})
+    }
+  }
+
+  handleOnMouseUp = () => {
+    this.setState({isMoving: false})
+  }
+
+  swapTiles = (from, to) => {
+    const {i, j} = from;
+    const {i: x, j: y} = to;
+
+    let tiles = this.state.tiles.slice();
+
+    let temp = tiles[i][j];
+    tiles[i][j] = tiles[x][y];
+    tiles[x][y] = temp;
+
+    this.setState({ tiles: tiles })
+  };
+
+  renderTiles = () => {
+    return this.state.tiles.map((row, i) => {
+      return row.map((tile, j) => {
+        const val = this.state.tiles[i][j];
+        return (
+          <Tile
+            className={val === 0 ? 'blank-tile' : 'tile'}
+            onMouseDown={() => this.handleOnMouseDown(i, j)}
+            onMouseEnter={() => this.handleOnMouseEnter(i, j)}
+            onMouseUp={() => this.handleOnMouseUp(i, j)}
+            key={val}
+            id={val}
+          />
+        )
+      })
+    })
+  }
+
+  render() {
+    return <div className="board">{this.renderTiles()}</div>
+  }
+}
